1448d84a20fa39c8078ac0d493d6850d3295eaeb
mjtech009 <mjtech009@gmail.com>
2023-08-04 10:54:15 +0530

    fix

--- action.sh
+++ action.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -ex
-timestamp=$(date +"%Y-%m-%d-%H-%M-%S")
-log_file="terraform_$timestamp.txt"
-exec &> "$log_file"
+
 # Function to apply Terraform configuration
 terraform init -upgrade
 terraform apply --auto-approve
